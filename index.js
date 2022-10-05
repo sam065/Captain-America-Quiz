10cafe7435870507045b6be11f4e7a1d74ab09de
Samadrita Ghosh <55602776+sam065@users.noreply.github.com>
2022-10-05 07:01:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+var readlineSync = require("readline-sync");
+
+// initialising score to 0
+var score = 0;
+
+// highScores database
+var highScores = [
+  {
+    name: "Bunty",
+    score: 1,
+  },
+
+  {
+    name: "Monty",
+    score: 2,
+  },
+  {
+    name: "Shonty",
+    score: 4,
+  },
+  {
+    name: "Tonty",
+    score: 5,
+  }
+]
+
+// array of objects containing questions and answers
+var questions = [{
+  question: "What is Captain America's weapon? ",
+  answer: "Shield"
+}, {
+  question: "What is the weapon made of? ",
+  answer: "Vibranium"
+},
+{
+  question: "Who made it for Captain?",
+  answer: "Howard Stark"
+},
+{
+  question: "What connection does the maker have with Tony Stark?",
+  answer: "Father"
+},
+{
+  question: "How old is Captain America?",
+  answer: "80 years"
+}];
+
+
+// taking input then displaying it
+function greetings() {
+  var playerName = readlineSync.question("Welcome to Captain America quiz. May I know your name? ");
+
+  console.log("Welcome " + playerName + "! Let's play this fun game to see if you know the Captain.");
+}
+
+function play(question, answer) {
+  // question is displayed to the user and demands an input
+  var userAnswer = readlineSync.question(question);
+
+  // if answers match : increment score
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log(" Correct ");
+    score = score + 1;
+    
+  // else : decrement score
+  } else {
+    console.log(" Wrong ");
+
+  }
+
+  console.log("current score: ", score);
+  console.log("- - - - - - - - ")
+}
+
+// playing the quiz
+function game() {
+  // traversing over the questions database : it is an array of objects
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    // passing the current question and answer to another function
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+// for displaying the final scores
+function displayScores() {
+  console.log("You SCORED: ", score);
+  console.log("The maximum possible score was 5");
+
+  
+  console.log("Score History : ");
+
+  // traversing over highScores database and displaying them all
+  highScores.map(score => console.log(score.name, " --> ", score.score))
+}
+
+
+greetings(); // calling the function to display welcome message
+game(); // calling the function to start the game 
+displayScores(); // calling the function to display final scores
